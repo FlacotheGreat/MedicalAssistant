872a0c98224e950847242b1899b9ceb222809aef
Anthony Perez <Perezanthony117@gmail.com>
2019-03-20 20:56:38 -0600

    Navigation menu

--- app/src/main/java/com/example/medicalassistant/MainActivity.java
+++ app/src/main/java/com/example/medicalassistant/MainActivity.java
@@ -77,13 +77,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_user) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_appointments) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_medication) {
 
         } else if (id == R.id.nav_share) {
 
