df028e340ac2744d595f370710fd1374b8a0f1df
Anthony Perez <Perezanthony117@gmail.com>
2019-04-18 20:56:05 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ app/src/main/java/com/example/medicalassistant/MyWorker.java
@@ -0,0 +1,57 @@
+package com.example.medicalassistant;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.util.Calendar;
+
+//import androidx.annotation.NonNull;
+//import androidx.work.Worker;
+//import androidx.work.WorkerParameters;
+
+//public class MyWorker {}extends Worker {
+//
+//
+//    public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+//        super(context, workerParams);
+//    }
+//
+//    @NonNull
+//    @Override
+//    public Result doWork() {
+//
+//        Log.d("TestWorker", "Day has changed");
+//        return null;
+//    }
+//
+//    void getCurrentDay(){
+//
+//        Calendar c =  Calendar.getInstance();
+//        int day = c.get(Calendar.DAY_OF_WEEK);
+//
+//        switch(day){
+//            case Calendar.SUNDAY:
+//                Constants.DAY = "Sunday";
+//                break;
+//            case Calendar.MONDAY:
+//                Constants.DAY = "Monday";
+//                break;
+//            case Calendar.TUESDAY:
+//                Constants.DAY = "Tuesday";
+//                break;
+//            case Calendar.WEDNESDAY:
+//                Constants.DAY = "Wednesday";
+//                break;
+//            case Calendar.THURSDAY:
+//                Constants.DAY = "Thursday";
+//                break;
+//            case Calendar.FRIDAY:
+//                Constants.DAY = "Friday";
+//                break;
+//            case Calendar.SATURDAY:
+//                Constants.DAY = "Saturday";
+//                break;
+//        }
+//
+//    }
+//}
